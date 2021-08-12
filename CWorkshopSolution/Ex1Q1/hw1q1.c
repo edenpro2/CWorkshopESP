657e7097567f6994d5264ab25c254fca9a488fc7
Eden Amiga <amiga@g.jct.ac.il>
2021-08-12 20:12:19 +0300

    Add Doxygen support

--- CWorkshopSolution/Ex1Q1/hw1q1.c
+++ CWorkshopSolution/Ex1Q1/hw1q1.c
@@ -8,17 +8,8 @@
 */
 
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
+#include "printChar.h"
 
-/// @brief Prints the input character from user unless it is q or Q
-void printChar();
-
-
-int main()
-{
-	printChar();
-}
 
 void printChar()
 {
---
+++ CWorkshopSolution/Ex1Q1/printChar.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef _PRINTCHAR_H_
+#define _PRINTCHAR_H_
+
+/// @file PrintChar function
+/// @brief Prints the input character from user unless it is q or Q
+void printChar();
+
+#endif
