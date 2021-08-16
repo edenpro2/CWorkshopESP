36b9b3469190e34a68ba478c26fdd4ad8015fae6
Eden Amiga <amiga@g.jct.ac.il>
2021-08-16 10:53:31 +0300

    ex1q2

--- CWorkshopSolution/Ex1Q2/PrintCharUpperCase.h
+++ CWorkshopSolution/Ex1Q2/PrintCharUpperCase.h
@@ -2,5 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+#ifndef _PRINTCHARUPPERCASE_H_
+#define _PRINTCHARUPPERCASE_H_
 /// @brief Prints the input character from user in uppercase unless it is q or Q
 void PrintCharUpperCase();
+
+#endif 
