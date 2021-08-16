b14e89a66343de5c70cd86e2c8ebc8ca77c2c2c9
Eden Amiga <amiga@g.jct.ac.il>
2021-08-16 11:03:35 +0300

    ex1q5

---
+++ CWorkshopSolution/ex1q5/CheckID.h
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#ifndef _CHECKID_H_
+#define _CHECKID_H_
+
+/// @brief Checks the id and tells whether it is valid or invalid based on input
+/// @param id number to check
+bool CheckID(int number);
+
+#endif 
