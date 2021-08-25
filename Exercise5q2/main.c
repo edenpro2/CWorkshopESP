bfdc61728505ea6a094c66be697aace12b2fb134
Eden Amiga <71780707+edenpro2@users.noreply.github.com>
2021-08-25 15:10:38 +0300

    süß

--- Exercise5q2/main.c
+++ Exercise5q2/main.c
@@ -3,13 +3,42 @@
 
 Complex getComplexNum()
 {
+	 int realOrComplex;
          printf("enter 0 for real or 1 for complex");  
-            
-         Complex temp
-         printf("Enter complex number");
-         Complex temp;
-         scanf("%d%h", temp.real, temp.imaginary);   
+         scanf("%d", &realOrComplex);
+	 Complex temp;
+	 if (!realOrComplex)
+	 {
+		printf("Enter real number");
+		temp.imaginary = 0; 
+		scanf("%d", temp.real);
+		 
+	 }
+	 else 
+	 {
+	        scanf("%d%h", temp.real, temp.imaginary);  
+	 }
+             
          return temp;
+}
+
+void PrintComplex(Complex res)
+{
+	if (result.imaginary > 0)
+	{
+		printf("%d + i%h", result.real, result.imaginary);
+	}
+	else
+	{
+		printf("%d - i%h", result.real, (0 - result.imaginary));
+	}        
+}
+
+void PrintComplexFraction(Complex_f fractionComplex)
+{
+	PrintComplex(fractionComplex.numerator);
+	printf(" / ");
+	PrintComplex(fractionComplex.denominator);
 }
 
 enum Operations
@@ -20,29 +49,34 @@ enum Operations
 int main()
 {
 	int input;
+	Complex A, B;
 
 	do
 	{
 		switch (input)
 		{
-                case EXIT:
-			break;
-
+                	A = getComplexNum();
+			B = getComplexNum();
 		case ADD:
-                     
-                        break;
+			PrintComplex(ComplexAdd(A,B));
+			break;
 		case SUB:
+			PrintComplex(ComplexSub(A,B));
 			break;
 		case MULT:
+			PrintComplex(ComplexMult(A,B));
 			break;
 		case DIV:
+			PrintComplexFraction(ComplexDiv(A,B));
 			break;
-		
+	        case EXIT:
+			break;
 		default:
 			printf("Number entered unrecognized");
 		}
 
 	} while (input != 5)
 
+	return 0;
 
 }
